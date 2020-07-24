54c20c861305722bf6386dcf0c24e3c1f6086b71
BOUZAIDIMOUAD <62447406+BOUZAIDIMOUAD@users.noreply.github.com>
2020-07-24 23:31:15 +0100

    Delete iddlo.py

--- iddlo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python
-#Importation de  NLTK
-import nltk
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
-from nltk.probability import FreqDist
-import matplotlib.pyplot as plt
